adbf0c06a96a24eb82877dda6e1670263e7bdde9
Ankush Girotra <ankush.girotra@gmail.com>
2023-01-13 23:19:52 -0600

    setup

---
+++ setup_env.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+## you just want to use the local typescript compiler in the node modules folder
+SCRIPT_DIR="$(pwd)"
+echo "guessing PROJECT ROOT: $(pwd)"
+
+PROJECT_BIN="$SCRIPT_DIR/nb/bin"
+export PATH="$PROJECT_BIN:$PATH"
+echo "finished adding $PROJECT_BIN to the top of your path"
+echo "\n"
+echo "---"
+
